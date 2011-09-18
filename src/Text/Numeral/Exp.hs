a45a43bf4dd60bb3cabc7822d134a01beae433c4
Roel van Dijk <vandijk.roel@gmail.com>
2011-09-18 23:37:30 +0200

    Removed the eval function

--- src/Text/Numeral/Exp.hs
+++ src/Text/Numeral/Exp.hs
@@ -3,11 +3,7 @@
            , UnicodeSyntax
   #-}
 
-module Text.Numeral.Exp
-    ( Exp(..)
-    , eval
-    , Side(L, R)
-    ) where
+module Text.Numeral.Exp ( Exp(..), Side(L, R) ) where
 
 
 -------------------------------------------------------------------------------
@@ -64,21 +60,6 @@ instance C.Mul Exp where mul = Mul
 instance C.Sub Exp where sub = Sub
 -- | Precisely the 'Scale' constructor.
 instance C.Scale Exp where scale = Scale
-
--- | Evaluates an expression to a value.
---
--- Law: @e == eval e@
-eval ∷ (C.Unknown α, C.Lit α, C.Neg α, C.Add α, C.Mul α, C.Sub α, C.Scale α) ⇒ Exp → α
-eval (Add x y)     = C.add (eval x) (eval y)
-eval (Mul x y)     = C.mul (eval x) (eval y)
-eval (Sub x y)     = C.sub (eval x) (eval y)
-eval (Neg x)       = C.neg (eval x)
-eval (Lit x)       = C.lit x
-eval (Scale b o r) = C.scale b o (eval r)
-eval Unknown       = C.unknown
-
--- prop_eval ∷ Exp → Bool
--- prop_eval e = e ≡ eval e
 
 
 -------------------------------------------------------------------------------
