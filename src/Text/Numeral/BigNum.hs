86ad9155b83c550dbac07394771d8ead5c5faea3
Roel van Dijk <vandijk.roel@gmail.com>
2011-09-21 16:22:09 +0200

    Made BigNum postfix names a function of their context

--- src/Text/Numeral/BigNum.hs
+++ src/Text/Numeral/BigNum.hs
@@ -11,8 +11,11 @@ module Text.Numeral.BigNum
   , symMap
   , forms
 
-  , scaleRepr, ordScaleRepr
-  , pelletierRepr, ordPelletierRepr
+  , scaleRepr
+  , pelletierRepr
+
+  , quantityName
+  , ordQuantityName
   ) where
 
 
@@ -104,66 +107,39 @@ forms t a1 a2 m1 m2 ctx =
 --------------------------------------------------------------------------------
 
 scaleRepr ∷ (IsString s, Monoid s)
-          ⇒ s -- ^Postfix for singular cardinal names.
-          → s -- ^Postfix for plural cardinal names.
+          ⇒ (Ctx Exp → s) -- ^Postfix.
           → [(ℤ, Ctx Exp → s)]
           → ScaleRepr s
-scaleRepr s p syms _ _ e ctx = (⊕ pf) <$> render repr e
+scaleRepr pf syms _ _ e ctx = (⊕ pf ctx) <$> render repr e
     where
-      pf = case ctx of
-             CtxMul _ (Lit 1) _ → s
-             CtxMul {}          → p
-             _                  → s
-      repr = cardinalRepr { reprValue = \n → M.lookup n syms' }
-      syms' = M.fromList syms ∪ symMap
-
-ordScaleRepr ∷ (IsString s, Monoid s)
-          ⇒ s -- ^Postfix for singular cardinal names.
-          → s -- ^Postfix for singular ordinal names.
-          → s -- ^Postfix for plural cardinal names.
-          → s -- ^Postifx for plural ordinal names.
-          → [(ℤ, Ctx Exp → s)]
-          → ScaleRepr s
-ordScaleRepr sc so pc po syms _ _ e ctx = (⊕ pf) <$> render repr e
-    where
-      pf = case ctx of
-             CtxMul _ (Lit 1) _ | outside   → so
-                                | otherwise → sc
-             CtxMul {}          | outside   → po
-                                | otherwise → pc
-             _                  | outside   → so
-                                | otherwise → sc
-      outside = isOutside R ctx
       repr = cardinalRepr { reprValue = \n → M.lookup n syms' }
       syms' = M.fromList syms ∪ symMap
 
 pelletierRepr ∷ (IsString s, Monoid s)
-              ⇒ s -- ^Postfix for singular cardinal offset 0 names.
-              → s -- ^Postfix for plural cardinal offset 0 names.
-              → s -- ^Postfix for singular cardinal offset 3 names.
-              → s -- ^Postfix for plural cardinal offset 3 names.
+              ⇒ (Ctx Exp → s) -- ^Postfix for offset 0 names.
+              → (Ctx Exp → s) -- ^Postfix for offset 3 names.
               → [(ℤ, Ctx Exp → s)]
               → ScaleRepr s
-pelletierRepr s0 p0 s3 p3 syms
-              b o e ctx | o ≡ 0 = scaleRepr s0 p0 syms b o e ctx
-                        | o ≡ 3 = scaleRepr s3 p3 syms b o e ctx
+pelletierRepr pf0 pf3 syms
+              b o e ctx | o ≡ 0 = scaleRepr pf0 syms b o e ctx
+                        | o ≡ 3 = scaleRepr pf3 syms b o e ctx
                         | otherwise = Nothing
 
-ordPelletierRepr ∷ (IsString s, Monoid s)
-                 ⇒ s -- ^Postfix for singular cardinal offset 0 names.
-                 → s -- ^Postfix for singular ordinal offset 0 names.
-                 → s -- ^Postfix for plural cardinal offset 0 names.
-                 → s -- ^Postfix for plural ordinal offset 0 names.
-                 → s -- ^Postfix for singular cardinal offset 3 names.
-                 → s -- ^Postfix for singular ordinal offset 3 names.
-                 → s -- ^Postfix for plural cardinal offset 3 names.
-                 → s -- ^Postfix for plural ordinal offset 3 names.
-                 → [(ℤ, Ctx Exp → s)]
-                 → ScaleRepr s
-ordPelletierRepr sc0 so0 pc0 po0
-                 sc3 so3 pc3 po3
-                 syms
-                 b o e ctx | o ≡ 0 = ordScaleRepr sc0 so0 pc0 po0 syms b o e ctx
-                           | o ≡ 3 = ordScaleRepr sc3 so3 pc3 po3 syms b o e ctx
-                           | otherwise = Nothing
+quantityName ∷ s → s → Ctx Exp → s
+quantityName s p ctx =
+    case ctx of
+      CtxMul _ (Lit 1) _ → s
+      CtxMul {}          → p
+      _                  → s
 
+ordQuantityName ∷ s → s → s → s → Ctx Exp → s
+ordQuantityName sc so pc po ctx =
+    case ctx of
+      CtxMul _ (Lit 1) _ | outside   → so
+                         | otherwise → sc
+      CtxMul {}          | outside   → po
+                         | otherwise → pc
+      _                  | outside   → so
+                         | otherwise → sc
+    where
+      outside = isOutside R ctx
