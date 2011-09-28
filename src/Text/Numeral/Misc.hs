a000176d639dfd718ef8668163fa9d0d17df339a
Roel van Dijk <vandijk.roel@gmail.com>
2011-09-29 00:15:00 +0200

    Degrade gracefully if integer-gmp is not availabe (GHC < 7.2.1)

--- src/Text/Numeral/Misc.hs
+++ src/Text/Numeral/Misc.hs
@@ -1,6 +1,6 @@
-{-# LANGUAGE MagicHash
+{-# LANGUAGE CPP
+           , MagicHash
            , NoImplicitPrelude
-           , OverloadedStrings
            , PackageImports
            , UnicodeSyntax
   #-}
@@ -11,10 +11,17 @@ module Text.Numeral.Misc where
 -- Imports
 --------------------------------------------------------------------------------
 
+import "base" Prelude ( Integral, (^) )
+#if __GLASGOW_HASKELL__ >= 702
 import "base" Data.Function ( ($) )
 import "base" GHC.Exts      ( Int(I#) )
-import "base" Prelude       ( Integral, fromIntegral, toInteger, (^) )
+import "base" Prelude       ( fromIntegral, toInteger )
 import "integer-gmp" GHC.Integer.Logarithms ( integerLogBase# )
+#else
+import "base" Data.Bool ( otherwise )
+import "base" Data.Ord  ( (<) )
+import "base" Prelude   ( div, (+), ($!), error )
+#endif
 
 --------------------------------------------------------------------------------
 -- Misc
@@ -28,4 +35,15 @@ dec = (10 ^)
 -- result must be able to fit in an ordinary Int value. This means the
 -- maximum input value is 10 ^ (maxBound ∷ Int).
 intLog ∷ (Integral α) ⇒ α → α
-intLog x = fromIntegral $ I# $ integerLogBase# 10 (toInteger x)
+-- GHC >= 7.2.x
+#if __GLASGOW_HASKELL__ >= 702
+intLog x = fromIntegral $ I# $ integerLogBase# 10 $ toInteger x
+#else
+intLog x | x < 0 = error "intLog: undefined for negative numbers"
+         | otherwise = go x 0
+    where
+      go n acc = case n `div` 10 of
+                   0 → acc
+                   1 → acc + 1
+                   q → go q $! acc + 1
+#endif
