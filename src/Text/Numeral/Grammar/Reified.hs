cd65faa746cb3978df62b9dce1c1641191196eda
Roel van Dijk <vandijk.roel@gmail.com>
2012-02-21 23:18:07 +0100

    Text.Numeral.Grammar - Added a lot of cases

--- src/Text/Numeral/Grammar/Reified.hs
+++ src/Text/Numeral/Grammar/Reified.hs
@@ -19,9 +19,11 @@ module Text.Numeral.Grammar.Reified
 -- Imports
 -------------------------------------------------------------------------------
 
-import "base" Data.Eq    ( Eq )
-import "base" Data.Maybe ( Maybe(Nothing, Just) )
-import "base" Text.Show  ( Show )
+import "base" Data.Bool     ( Bool(False, True) )
+import "base" Data.Eq       ( Eq )
+import "base" Data.Function ( ($) )
+import "base" Data.Maybe    ( Maybe(Nothing, Just) )
+import "base" Text.Show     ( Show )
 import "base-unicode-symbols" Data.Eq.Unicode ( (≡) )
 import qualified "this" Text.Numeral.Grammar as G
 
@@ -45,15 +47,36 @@ defaultInflection = Inflection Nothing Nothing Nothing
 -- Grammatical categories
 -------------------------------------------------------------------------------
 
-data Case = Nominative
-          | Accusative
-          | Dative
+data Case = Abessive
           | Ablative
+          | Accusative
+          | Comitative
+          | Dative
+          | Delative
+          | Distributive
+          | DistributiveTemporal
+          | Essive
           | Genitive
-          | Vocative
-          | Locative
           | Instrumental
+          | Instructive
+          | Lative
+          | Locative (Maybe Locative)
+          | Multiplicative
+          | Nominative
+          | Partitive
+          | Sublative
+          | SuperEssive
+          | Translative
+          | Vocative
             deriving (Eq, Show)
+
+data Locative = LocativeIllative
+              | LocativeInessive
+              | LocativeElative
+              | LocativeAllative
+              | LocativeAdessive
+              | LocativeAblative
+                deriving (Eq, Show)
 
 data Gender = Neuter
             | Masculine
@@ -77,37 +100,127 @@ instance G.NoCase Inflection where
     noCase inf = inf { iCase = Nothing }
     hasNoCase (Inflection {iCase}) = iCase ≡ Nothing
 
-instance G.Nominative Inflection where
-    nominative inf = inf { iCase = Just Nominative }
-    isNominative (Inflection {iCase}) = iCase ≡ Just Nominative
+instance G.Ablative Inflection where
+    ablative inf = inf { iCase = Just Ablative }
+    isAblative (Inflection {iCase}) = iCase ≡ Just Ablative
+
+instance G.Abessive Inflection where
+    abessive inf = inf { iCase = Just Abessive }
+    isAbessive (Inflection {iCase}) = iCase ≡ Just Abessive
 
 instance G.Accusative Inflection where
     accusative inf = inf { iCase = Just Accusative }
     isAccusative (Inflection {iCase}) = iCase ≡ Just Accusative
 
+instance G.Comitative Inflection where
+    comitative inf = inf { iCase = Just Comitative }
+    isComitative (Inflection {iCase}) = iCase ≡ Just Comitative
+
 instance G.Dative Inflection where
     dative inf = inf { iCase = Just Dative }
     isDative (Inflection {iCase}) = iCase ≡ Just Dative
 
-instance G.Ablative Inflection where
-    ablative inf = inf { iCase = Just Ablative }
-    isAblative (Inflection {iCase}) = iCase ≡ Just Ablative
+instance G.Delative Inflection where
+    delative inf = inf { iCase = Just Delative }
+    isDelative (Inflection {iCase}) = iCase ≡ Just Delative
+
+instance G.Distributive Inflection where
+    distributive inf = inf { iCase = Just Distributive }
+    isDistributive (Inflection {iCase}) = iCase ≡ Just Distributive
+
+instance G.DistributiveTemporal Inflection where
+    distributiveTemporal inf = inf { iCase = Just DistributiveTemporal }
+    isDistributiveTemporal (Inflection {iCase}) = iCase ≡ Just DistributiveTemporal
+
+instance G.Essive Inflection where
+    essive inf = inf { iCase = Just Essive }
+    isEssive (Inflection {iCase}) = iCase ≡ Just Essive
 
 instance G.Genitive Inflection where
     genitive inf = inf { iCase = Just Genitive }
     isGenitive (Inflection {iCase}) = iCase ≡ Just Genitive
 
+instance G.Instrumental Inflection where
+    instrumental inf = inf { iCase = Just Instrumental }
+    isInstrumental (Inflection {iCase}) = iCase ≡ Just Instrumental
+
+instance G.Instructive Inflection where
+    instructive inf = inf { iCase = Just Instructive }
+    isInstructive (Inflection {iCase}) = iCase ≡ Just Instructive
+
+instance G.Lative Inflection where
+    lative inf = inf { iCase = Just Lative }
+    isLative (Inflection {iCase}) = iCase ≡ Just Lative
+
+instance G.Locative Inflection where
+    locative inf = inf { iCase = Just (Locative Nothing) }
+    isLocative (Inflection {iCase}) =
+        case iCase of
+          Just (Locative _) → True
+          _                 → False
+
+instance G.Multiplicative Inflection where
+    multiplicative inf = inf { iCase = Just Multiplicative }
+    isMultiplicative (Inflection {iCase}) = iCase ≡ Just Multiplicative
+
+instance G.Nominative Inflection where
+    nominative inf = inf { iCase = Just Nominative }
+    isNominative (Inflection {iCase}) = iCase ≡ Just Nominative
+
+instance G.Partitive Inflection where
+    partitive inf = inf { iCase = Just Partitive }
+    isPartitive (Inflection {iCase}) = iCase ≡ Just Partitive
+
+instance G.SuperEssive Inflection where
+    superEssive inf = inf { iCase = Just SuperEssive }
+    isSuperEssive (Inflection {iCase}) = iCase ≡ Just SuperEssive
+
+instance G.Sublative Inflection where
+    sublative inf = inf { iCase = Just Sublative }
+    isSublative (Inflection {iCase}) = iCase ≡ Just Sublative
+
+instance G.Translative Inflection where
+    translative inf = inf { iCase = Just Translative }
+    isTranslative (Inflection {iCase}) = iCase ≡ Just Translative
+
 instance G.Vocative Inflection where
     vocative inf = inf { iCase = Just Vocative }
     isVocative (Inflection {iCase}) = iCase ≡ Just Vocative
 
-instance G.Locative Inflection where
-    locative inf = inf { iCase = Just Locative }
-    isLocative (Inflection {iCase}) = iCase ≡ Just Locative
 
-instance G.Instrumental Inflection where
-    instrumental inf = inf { iCase = Just Instrumental }
-    isInstrumental (Inflection {iCase}) = iCase ≡ Just Instrumental
+-------------------------------------------------------------------------------
+-- Locative case instances
+-------------------------------------------------------------------------------
+
+instance G.LocativeInessive Inflection where
+    locativeInessive inf = inf {iCase = Just $ Locative $ Just LocativeInessive}
+    isLocativeInessive (Inflection {iCase}) =
+        iCase ≡ (Just $ Locative $ Just LocativeInessive)
+
+instance G.LocativeElative Inflection where
+    locativeElative inf = inf {iCase = Just $ Locative $ Just LocativeElative}
+    isLocativeElative (Inflection {iCase}) =
+        iCase ≡ (Just $ Locative $ Just LocativeElative)
+
+instance G.LocativeIllative Inflection where
+    locativeIllative inf = inf {iCase = Just $ Locative $ Just LocativeIllative}
+    isLocativeIllative (Inflection {iCase}) =
+        iCase ≡ (Just $ Locative $ Just LocativeIllative)
+
+instance G.LocativeAdessive Inflection where
+    locativeAdessive inf = inf {iCase = Just $ Locative $ Just LocativeAdessive}
+    isLocativeAdessive (Inflection {iCase}) =
+        iCase ≡ (Just $ Locative $ Just LocativeAdessive)
+
+instance G.LocativeAblative Inflection where
+    locativeAblative inf = inf {iCase = Just $ Locative $ Just LocativeAblative}
+    isLocativeAblative (Inflection {iCase}) =
+        iCase ≡ (Just $ Locative $ Just LocativeAblative)
+
+instance G.LocativeAllative Inflection where
+    locativeAllative inf = inf {iCase = Just $ Locative $ Just LocativeAllative}
+    isLocativeAllative (Inflection {iCase}) =
+        iCase ≡ (Just $ Locative $ Just LocativeAllative)
 
 
 -------------------------------------------------------------------------------
